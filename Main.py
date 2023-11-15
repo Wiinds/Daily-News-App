c0c311cfbeab41b829e39d9be5e2ef9191050231
Jeremy Abraham <jeremyabraham17@gmail.com>
2023-11-15 12:59:45 -0500

    Small Changes to API

--- Main.py
+++ Main.py
@@ -3,10 +3,10 @@ import smtplib, ssl
 import os 
 from send_emailv2 import send_email 
 
+api_key = os.getenv("Daily_News_API")
 
-api_key = "5dc27eea6fef477a92581918563993b8"
 
-url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5dc27eea6fef477a92581918563993b8"
+url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api_key}"
 
 # make request
 response = requests.get(url)
